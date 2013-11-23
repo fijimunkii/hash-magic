dac2955ee3193ee38168aadc150fa902a38368f3
Jonathan Leung <jon@j.local>
2013-11-22 21:42:16 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HashMagic::Application.config.secret_token = 'd7c6cb3ca8b46330bddabb5b35e55b160aa58333b9e867e204a6f9129ae3428c9721a3aeddef8958c36bc730d4340bf8fb25489563b4a07478d91f4e460430c3'
