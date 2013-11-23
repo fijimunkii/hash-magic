f5c5223d3173fd4c2fbb05b70b48769cbbc06a70
Jonathan Leung <jon@j.local>
2013-11-23 01:52:39 -0500

    add .js at the end of things...

--- config/application.rb
+++ config/application.rb
@@ -58,8 +58,6 @@ module HashMagic
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.assets.compile = true
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
