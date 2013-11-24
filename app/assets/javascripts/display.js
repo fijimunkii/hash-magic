2c49db6b49ea937765c1a33c0bdd04842a9a3f8c
Harrison Powers <fijimunkii@gmail.com>
2013-11-24 12:32:18 -0500

    qr code

--- app/assets/javascripts/display.js
+++ app/assets/javascripts/display.js
@@ -43,11 +43,33 @@ Display.pollServer = function() {
   }, Display.POLL_INTERVAL);
 }
 
+Display.qrCode = function() {
+  var base = 'http://qrickit.com/api/qr?d=',
+      uri = 'http://magicwand.herokuapp.com',
+      text = '&addtext=Visit+On+A+Mobile+Device',
+      textcolor = '&txtcolor=442EFF',
+      fgdcolor = '&fgdcolor=76103C',
+      bgdcolor = '&bgdcolor=C0F912',
+      qrsize = '&qrsize=150',
+      fileType = '&t=p',
+      errorCorrection = '&e=h';
+  //http://qrickit.com/api/qr?d=http://anyurl&addtext=Hello+World&txtcolor=442EFF&fgdcolor=76103C&bgdcolor=C0F912&qrsize=150&t=p&e=m
+
+  var imgCode = base + uri + text + textcolor + fgdcolor + bgdcolor + qrsize + fileType + errorCorrection;
+
+  return imgCode;
+}
+
 Display.setup = function() {
 
   $('body').addClass('not-active');
 
   $('head').append('<link rel="stylesheet" type="text/css" href="/stylesheets/display.css">');
+
+  $qrCode = $('<img>')
+  .addClass('qr-code')
+  .attr('src', Display.qrCode())
+  .appendTo($('body'));
 
   Display.pollServer();
 
