97ac5e700c3a7a5c888c0d3a4146693a759755f7
Harrison Powers <fijimunkii@gmail.com>
2013-11-28 12:40:21 -0500

    removed pry binding

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -4,7 +4,6 @@ class DisplaysController < ApplicationController
   end
 
   def listen
-    binding.pry
     should_trigger = Trigger.active?(params[:id], session[:_csrf_token])
     render json: { trigger: should_trigger }
   end
